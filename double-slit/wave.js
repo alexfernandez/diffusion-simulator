40fa7d223fd734c23da8abfe3699255ded65594f
Alex Fernández <alexfernandeznpm@gmail.com>
2021-09-04 01:35:54 +0200

    Use class

--- double-slit/wave.js
+++ double-slit/wave.js
@@ -1,98 +1,27 @@
 'use strict'
 
 let time = 0, speedms = 0, damping = 0, sizem = 50
-let width, height, cx, cy, propagation
 const period = 10
 const dt = 0.05
 const fontSize = 16
-let ctx, updater, raw
-let grid0 = []
-let grid1 = []
-let grid2 = []
+
+
 
 window.onload = () => {
-	resetSimulation()
-	draw()
+	const canvas = document.getElementById('canvas');
+	const ctx = canvas.getContext('2d');
+	ctx.font = '16px sans-serif'
+	ctx.clearRect(0, 0, this.width, this.height)
+	const simulator = new Simulator(canvas.width, canvas.height - fontSize, ctx)
+	simulator.reset()
+	simulator.draw()
 	if (getCheckbox('autorun')) {
 		console.log('running')
-		run()
+		simulator.run()
 	}
-	document.getElementById('run').onclick = run
-	document.getElementById('pause').onclick = pause
-	document.getElementById('reset').onclick = reset
-}
-
-function run() {
-	if (propagation > 0.5) {
-		alert(`Propagation ${propagation} too big > 0.5, aborting`)
-		return
-	}
-	if (updater) return
-	let total = 0
-	let rounds = 0
-	updater = window.setInterval(() => {
-		const start = Date.now()
-		advance()
-		draw()
-		replace()
-		const elapsed = Date.now() - start
-		total += elapsed
-		rounds += 1
-	}, dt * 1000)
-	window.setInterval(() => {
-		console.log(`Average ms per round: ${Math.round(total / rounds)}`)
-	}, 1000)
-}
-
-function pause() {
-	if (!updater) return
-	window.clearInterval(updater)
-	updater = null
-}
-
-function reset() {
-	pause()
-	resetSimulation()
-	draw()
-}
-
-function resetSimulation() {
-	console.log('resetting')
-	time = 0
-	const canvas = document.getElementById('canvas');
-	width = canvas.width
-	height = canvas.height - fontSize
-	cx = Math.round(width / 2)
-	cy = Math.round(height / 2)
-	ctx = canvas.getContext('2d');
-	ctx.font = '16px sans-serif'
-	ctx.clearRect(0, 0, width, height)
-	raw = ctx.getImageData(0, 0, width, height);
-	speedms = getParameter('speed')
-	damping = getParameter('damping')
-	propagation = computePropagation()
-	grid0 = createGrid()
-	grid1 = createGrid()
-	grid2 = createGrid()
-	console.log('propagation ', propagation)
-	console.log('reset')
-}
-
-function computePropagation() {
-	const dx = sizem / width
-	const interval = dt * speedms / dx
-	return interval * interval
-}
-
-function createGrid() {
-	const grid = []
-	for (let i = -1; i <= width; i++) {
-		grid[i] = []
-		for (let j = -1; j <= height; j++) {
-			grid[i][j] = 0
-		}
-	}
-	return grid
+	document.getElementById('run').onclick = () => simulator.run()
+	document.getElementById('pause').onclick = () => simulator.pause()
+	document.getElementById('reset').onclick = () => simulator.reset()
 }
 
 function getParameter(name) {
@@ -103,120 +32,197 @@ function getCheckbox(name) {
 	return document.getElementById(name).checked
 }
 
-function advance() {
-	time += dt
-	for (let i = 0; i < width; i++) {
-		for (let j = 0; j < height; j++) {
-			grid2[i][j] = computeNext(i, j)
+class Simulator {
+
+	constructor(width, height, ctx) {
+		this.width = width
+		this.height = height
+		this.cx = Math.round(width / 2)
+		this.cy = Math.round(height / 2)
+		this.grid0 = this.createGrid()
+		this.grid1 = this.createGrid()
+		this.grid2 = this.createGrid()
+		this.propagation = 0
+		this.updater = null
+		this.logger = null
+		this.ctx = ctx
+		this.raw = ctx.getImageData(0, 0, this.width, this.height);
+	}
+
+	run() {
+		if (this.propagation > 0.5) {
+			alert(`Propagation ${this.propagation} too big > 0.5, aborting`)
+			return
+		}
+		if (this.updater) return
+		let total = 0
+		let rounds = 0
+		this.updater = window.setInterval(() => {
+			const start = Date.now()
+			this.advance()
+			this.draw()
+			this.replace()
+			const elapsed = Date.now() - start
+			total += elapsed
+			rounds += 1
+		}, dt * 1000)
+		this.logger = window.setInterval(() => {
+			console.log(`Average ms per round: ${Math.round(total / rounds)}`)
+		}, 1000)
+	}
+
+	pause() {
+		if (!this.updater) return
+		window.clearInterval(this.updater)
+		window.clearInterval(this.logger)
+		this.updater = null
+		this.logger = null
+	}
+
+	reset() {
+		this.pause()
+		console.log('resetting')
+		time = 0
+		speedms = getParameter('speed')
+		damping = getParameter('damping')
+		this.propagation = this.computePropagation()
+		console.log('propagation ', this.propagation)
+		console.log('reset')
+		this.draw()
+	}
+
+	computePropagation() {
+		const dx = sizem / this.width
+		const interval = dt * speedms / dx
+		return interval * interval
+	}
+
+	createGrid() {
+		const grid = []
+		for (let i = -1; i <= this.width; i++) {
+			grid[i] = []
+			for (let j = -1; j <= this.height; j++) {
+				grid[i][j] = 0
+			}
+		}
+		return grid
+	}
+
+	advance() {
+		time += dt
+		for (let i = 0; i < this.width; i++) {
+			for (let j = 0; j < this.height; j++) {
+				this.grid2[i][j] = this.computeNext(i, j)
+			}
+		}
+		this.wrapup()
+		if (time < period) {
+			this.grid2[this.cx][this.cy] = Math.sin(2 * Math.PI * time / period)
 		}
 	}
-	wrapup()
-	if (time < period) {
-		grid2[cx][cy] = Math.sin(2 * Math.PI * time / period)
-	}
-}
 
-function computeNext(i, j) {
-	const localDamping = damping //computeDamping(i, j)
-	const previous = grid1[i][j]
-	const damped = (1 - localDamping * dt) * (previous - grid0[i][j])
-	const neighbors = grid1[i + 1][j] + grid1[i - 1][j] + grid1[i][j + 1] + grid1[i][j - 1]
-	const influence = propagation * (neighbors - 4 * previous)
-	return previous + damped + influence
-}
+	computeNext(i, j) {
+		const localDamping = damping //computeDamping(i, j)
+		const previous = this.grid1[i][j]
+		const damped = (1 - localDamping * dt) * (previous - this.grid0[i][j])
+		const neighbors = this.grid1[i + 1][j] + this.grid1[i - 1][j] + this.grid1[i][j + 1] + this.grid1[i][j - 1]
+		const influence = this.propagation * (neighbors - 4 * previous)
+		return previous + damped + influence
+	}
 
-function computeDamping(i, j) {
-	const lowratio = 0.25
-	const highratio = 0.30
-	const distancex = Math.abs(cx - i) / width
-	const distancey = Math.abs(cy - j) / height
-	if (distancex > highratio || distancey > highratio) {
-		return 1
+	computeDamping(i, j) {
+		const lowratio = 0.25
+		const highratio = 0.30
+		const distancex = Math.abs(this.cx - i) / this.width
+		const distancey = Math.abs(this.cy - j) / this.height
+		if (distancex > highratio || distancey > highratio) {
+			return 1
+		}
+		if (distancex < lowratio && distancey < lowratio) {
+			return damping
+		}
+		const distance = Math.max(distancex, distancey)
+		return (distance - lowratio) / (highratio - lowratio)
 	}
-	if (distancex < lowratio && distancey < lowratio) {
-		return damping
-	}
-	const distance = Math.max(distancex, distancey)
-	return (distance - lowratio) / (highratio - lowratio)
-}
 
-function wrapup() {
-	wrapupZero()
-}
+	wrapup() {
+		this.wrapupZero()
+	}
 
-function wrapupZero() {
-	for (let i = -1; i <= width; i++) {
-		grid2[i][-1] = 0
-		grid2[i][height] = 0
-	}
-	for (let j = -1; j <= height; j++) {
-		grid2[-1][j] = 0
-		grid2[width][j] = 0
-		grid2[width - 1][j]
-	}
-}
-
-function wrapupEqual() {
-	for (let i = 0; i <= width - 1; i++) {
-		grid2[i][-1] = grid2[i][0]
-		grid2[i][height] = grid2[i][height - 1]
-	}
-	for (let j = 0; j <= height - 1; j++) {
-		grid2[-1][j] = grid2[0][j]
-		grid2[width][j] = grid2[width - 1][j]
-	}
-	grid2[-1][-1] = grid2[0][0]
-	grid2[-1][height] = grid2[0][height - 1]
-	grid2[width][-1] = grid2[width - 1][0]
-	grid2[width][height] = grid2[width - 1][height - 1]
-}
-
-function wrapupSecond() {
-	for (let i = 0; i <= width - 1; i++) {
-		grid2[i][-1] = 2 * grid2[i][0] - grid2[i][1]
-		grid2[i][height] = 2 * grid2[i][height - 1] - grid2[i][height - 2]
-	}
-	for (let j = 0; j <= height - 1; j++) {
-		grid2[-1][j] = 2 * grid2[0][j] - grid2[1][j]
-		grid2[width][j] = 2 * grid2[width - 1][j] - grid2[width - 2][j]
-	}
-}
-
-function draw() {
-	ctx.clearRect(0, height, width, height + fontSize)
-	for (let i = 0; i < width; i++) {
-		for (let j = 0; j < height; j++) {
-			setPixel(i, j, grid2[i][j])
+	wrapupZero() {
+		for (let i = -1; i <= this.width; i++) {
+			this.grid2[i][-1] = 0
+			this.grid2[i][this.height] = 0
+		}
+		for (let j = -1; j <= this.height; j++) {
+			this.grid2[-1][j] = 0
+			this.grid2[this.width][j] = 0
+			this.grid2[this.width - 1][j]
 		}
 	}
-	ctx.putImageData(raw, 0, 0);
-	ctx.fillText('t = ' + time.toFixed(1) + ' s', 100, height + fontSize - 1)
-	//console.log(grid2[cx + 1][cy])
-}
 
-function replace() {
-	const recycled = grid0
-	grid0 = grid1
-	grid1 = grid2
-	grid2 = recycled
-}
+	wrapupEqual() {
+		for (let i = 0; i <= this.width - 1; i++) {
+			this.grid2[i][-1] = this.grid2[i][0]
+			this.grid2[i][this.height] = this.grid2[i][this.height - 1]
+		}
+		for (let j = 0; j <= this.height - 1; j++) {
+			this.grid2[-1][j] = this.grid2[0][j]
+			this.grid2[this.width][j] = this.grid2[this.width - 1][j]
+		}
+		this.grid2[-1][-1] = this.grid2[0][0]
+		this.grid2[-1][this.height] = this.grid2[0][this.height - 1]
+		this.grid2[this.width][-1] = this.grid2[this.width - 1][0]
+		this.grid2[this.width][this.height] = this.grid2[this.width - 1][this.height - 1]
+	}
 
-function setPixel(x, y, value) {
-	const index = (x + y * width) * 4
-	if (value > 1 || value < -1) {
-		raw.data[index] = 0
-		raw.data[index + 1] = 0
-		raw.data[index + 2] = 0
+	wrapupSecond() {
+		for (let i = 0; i <= this.width - 1; i++) {
+			this.grid2[i][-1] = 2 * this.grid2[i][0] - this.grid2[i][1]
+			this.grid2[i][this.height] = 2 * this.grid2[i][this.height - 1] - this.grid2[i][this.height - 2]
+		}
+		for (let j = 0; j <= this.height - 1; j++) {
+			this.grid2[-1][j] = 2 * this.grid2[0][j] - this.grid2[1][j]
+			this.grid2[this.width][j] = 2 * this.grid2[this.width - 1][j] - this.grid2[this.width - 2][j]
+		}
 	}
-	else if (value >= 0) {
-		raw.data[index] = 255
-		raw.data[index + 1] = (1 - value) * 255
-		raw.data[index + 2] = (1 - value) * 255
-	} else {
-		raw.data[index] = (1 + value) * 255
-		raw.data[index + 1] = 255
-		raw.data[index + 2] = (1 + value) * 255
+
+	draw() {
+		this.ctx.clearRect(0, this.height, this.width, this.height + fontSize)
+		for (let i = 0; i < this.width; i++) {
+			for (let j = 0; j < this.height; j++) {
+				this.setPixel(i, j, this.grid2[i][j])
+			}
+		}
+		this.ctx.putImageData(this.raw, 0, 0);
+		this.ctx.fillText('t = ' + time.toFixed(1) + ' s', 100, this.height + fontSize - 1)
+		//console.log(this.grid2[this.cx + 1][this.cy])
 	}
-	raw.data[index + 3] = 255
+
+	replace() {
+		const recycled = this.grid0
+		this.grid0 = this.grid1
+		this.grid1 = this.grid2
+		this.grid2 = recycled
+	}
+
+	setPixel(x, y, value) {
+		const index = (x + y * this.width) * 4
+		if (value > 1 || value < -1) {
+			this.raw.data[index] = 0
+			this.raw.data[index + 1] = 0
+			this.raw.data[index + 2] = 0
+		}
+		else if (value >= 0) {
+			this.raw.data[index] = 255
+			this.raw.data[index + 1] = (1 - value) * 255
+			this.raw.data[index + 2] = (1 - value) * 255
+		} else {
+			this.raw.data[index] = (1 + value) * 255
+			this.raw.data[index + 1] = 255
+			this.raw.data[index + 2] = (1 + value) * 255
+		}
+		this.raw.data[index + 3] = 255
+	}
 }
 
