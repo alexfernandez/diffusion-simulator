d67587fa9794ccc0091ad6da94a0fccad7a8af3b
Alex Fernández <alexfernandeznpm@gmail.com>
2021-09-20 01:32:28 +0200

    Implemented Schroedinger simulation

--- double-slit/schroedinger.js
+++ double-slit/schroedinger.js
@@ -3,12 +3,10 @@
 
 class SchroedingerParameters {
 	constructor() {
-		this.sizem = 50
-		this.periodSeconds = 1
+		this.dx = 1
 		this.dt = 0.02
 		this.offscreenDamping = 0.99
 		this.offscreenBuffer = 200
-		this.amplitude = 10
 		this.maxSpeedRuns = 10
 	}
 }
@@ -41,43 +39,37 @@ class SchroedingerSimulator {
 		this.height = this.screenHeight + 2 * this.parameters.offscreenBuffer
 		this.cx = Math.round(this.width / 2)
 		this.cy = Math.round(this.height / 2)
-		this.grid0 = this.createGrid()
-		this.grid1 = this.createGrid()
-		this.grid2 = this.createGrid()
-		this.damping = this.createGrid()
-		this.barrier = this.createGrid()
+		this.r0 = this.createGrid()
+		this.r1 = this.createGrid()
+		this.i05 = this.createGrid()
+		this.i15 = this.createGrid()
+		this.potential = this.createGrid()
 		this.propagation = 0
 		this.ctx = ctx
 		this.raw = ctx.getImageData(0, 0, this.width, this.height)
 		this.time = 0
-		this.speedms = 0
-		this.totalPeriods = 0
+		this.mass = 0
+		this.totalEnergy = 0
 		this.readY = 0
 	}
 
 	reset() {
-		this.fillGrid(this.grid0, 0)
-		this.fillGrid(this.grid1, 0)
-		this.fillGrid(this.grid2, 0)
-		this.fillGrid(this.barrier, 0)
+		this.fillGrid(this.r0, 0)
+		this.fillGrid(this.r1, 0)
+		this.fillGrid(this.i05, 0)
+		this.fillGrid(this.i15, 0)
+		this.fillGrid(this.potential, 0)
 		this.time = 0
-		this.speedms = getParameter('schroedinger-speed')
-		this.initialDamping = getParameter('schroedinger-damping')
-		this.totalPeriods = getParameter('schroedinger-periods')
-		this.propagation = this.computePropagation()
-		console.log('propagation ', this.propagation)
-		this.computeDampingField()
-		this.computeBarrier()
+		this.mass = getParameter('schroedinger-mass')
+		this.totalEnergy = getParameter('schroedinger-energy')
+		this.computeInitialField()
+		this.computePotential()
 		this.readY = this.parameters.offscreenBuffer + this.screenHeight - 1
 		console.log(`graphing: ${this.readY}`)
 		this.draw()
 	}
 
 	isValid() {
-		if (this.propagation > 0.5) {
-			alert(`Propagation ${this.propagation} too big > 0.5, aborting`)
-			return false
-		}
 		return true
 	}
 
@@ -87,40 +79,37 @@ class SchroedingerSimulator {
 		this.replace()
 	}
 
-	computePropagation() {
-		const dx = this.parameters.sizem / this.width
-		const interval = this.parameters.dt * this.speedms / dx
-		return interval * interval
+	computeInitialField() {
+		const j = this.parameters.offscreenBuffer + this.screenHeight / 10
+		const index = this.cx + j * this.width
+		this.r0[index] = this.totalEnergy
+		this.i05[index + this.width] = this.totalEnergy
 	}
 
-	computeDampingField() {
-		this.fillGrid(this.damping, this.initialDamping)
-		let transparency = 1
-		for (let s = 0; s < this.parameters.offscreenBuffer; s++) {
-			const d = this.parameters.offscreenBuffer - s
-			transparency *= this.parameters.offscreenDamping
-			for (let j = d; j < this.height - d; j++) {
-				this.damping[d + j * this.width] = 1 - transparency
-				this.damping[this.width - d + j * this.width] = 1 - transparency
-			}
-			for (let i = d; i < this.width - d; i++) {
-				this.damping[i + d * this.width] = 1 - transparency
-				this.damping[i + (this.height - d) * this.width] = 1 - transparency
-			}
-		}
-	}
-
-	computeBarrier() {
+	computePotential() {
 		const firstY = this.parameters.offscreenBuffer + this.screenHeight / 5
 		const secondY = this.parameters.offscreenBuffer + 3 * this.screenHeight / 5
 		console.log(`barriers: ${firstY}, ${secondY}`)
 		for (let i = 0; i < this.width; i++) {
 			const diff = Math.abs(this.cx - i)
 			if (diff > slitWidth / 2) {
-				this.barrier[i + firstY * this.width] = 1
+				this.potential[i + firstY * this.width] = 1
 			}
 			if (this.isSecondBarrier(i)) {
-				this.barrier[i + secondY * this.width] = 1
+				this.potential[i + secondY * this.width] = 1
+			}
+		}
+		let transparency = 1
+		for (let s = 0; s < this.parameters.offscreenBuffer; s++) {
+			const d = this.parameters.offscreenBuffer - s
+			transparency *= this.parameters.offscreenDamping
+			for (let j = d; j < this.height - d; j++) {
+				this.potential[d + j * this.width] = 1 - transparency
+				this.potential[this.width - d + j * this.width] = 1 - transparency
+			}
+			for (let i = d; i < this.width - d; i++) {
+				this.potential[i + d * this.width] = 1 - transparency
+				this.potential[i + (this.height - d) * this.width] = 1 - transparency
 			}
 		}
 	}
@@ -150,26 +139,28 @@ class SchroedingerSimulator {
 		this.time += this.parameters.dt
 		for (let i = 1; i < this.width - 1; i++) {
 			for (let j = 1; j < this.height - 1; j++) {
-				this.grid2[i + j * this.width] = this.computeNext(i, j)
+				this.r1[i + j * this.width] = this.computeNextR(i, j)
+				this.i15[i + j * this.width] = this.computeNextI(i, j)
 			}
-		}
-		if (!this.totalPeriods || this.time < this.parameters.periodSeconds * this.totalPeriods) {
-			const j = this.parameters.offscreenBuffer + this.screenHeight / 10
-			this.grid2[this.cx + j * this.width] = this.parameters.amplitude * Math.sin(2 * Math.PI * this.time / this.parameters.periodSeconds)
 		}
 	}
 
-	computeNext(i, j) {
+	computeNextR(i, j) {
 		const index = i + j * this.width
-		if (this.barrier[index]) {
-			return 0
-		}
-		const previous = this.grid1[index]
-		const damping = this.damping[index]
-		const damped = (1 - damping * this.parameters.dt) * (previous - this.grid0[index])
-		const neighbors = this.grid1[index + 1] + this.grid1[index - 1] + this.grid1[index + this.width] + this.grid1[index - this.width]
-		const influence = this.propagation * (neighbors - 4 * previous)
-		return previous + damped + influence
+		const previous = this.i05[index]
+		const neighbors = this.i05[index + 1] + this.i05[index - 1] + this.i05[index + this.width] + this.i05[index - this.width]
+		const constant = -1 / (2 * this.mass) / (this.parameters.dx * this.parameters.dx)
+		const hi = constant * (neighbors - 4 * previous) + this.potential[index] * previous
+		return this.r0[index] + this.parameters.dt * hi
+	}
+
+	computeNextI(i, j) {
+		const index = i + j * this.width
+		const previous = this.r1[index]
+		const neighbors = this.r1[index + 1] + this.r1[index - 1] + this.r1[index + this.width] + this.r1[index - this.width]
+		const constant = -1 / (2 * this.mass) / (this.parameters.dx * this.parameters.dx)
+		const hr = constant * (neighbors - 4 * previous) + this.potential[index] * previous
+		return this.i05[index] - this.parameters.dt * hr
 	}
 
 	draw() {
@@ -181,21 +172,22 @@ class SchroedingerSimulator {
 		this.ctx.putImageData(this.raw, 0, 0);
 		this.ctx.clearRect(0, this.height, this.width, this.height + fontSize)
 		this.ctx.fillText('t = ' + this.time.toFixed(1) + ' s', this.screenWidth / 3, this.screenHeight + fontSize - 1)
-		//console.log(this.grid2[this.cx + 1 + this.cy * this.width])
 	}
 
 	replace() {
-		const recycled = this.grid0
-		this.grid0 = this.grid1
-		this.grid1 = this.grid2
-		this.grid2 = recycled
+		const recycledR = this.r0
+		this.r0 = this.r1
+		this.r1 = recycledR
+		const recycledI = this.i05
+		this.i05 = this.i15
+		this.i15 = recycledI
 	}
 
 	setPixel(i, j) {
 		const index = i + this.parameters.offscreenBuffer + (j + this.parameters.offscreenBuffer) * this.width
-		const value = this.grid2[index]
+		const value = this.getProb(index)
 		const position = (i + j * this.width) * 4
-		if (this.barrier[index]) {
+		if (this.potential[index]) {
 			this.raw.data[position] = 0
 			this.raw.data[position + 1] = 0
 			this.raw.data[position + 2] = 0
@@ -222,6 +214,10 @@ class SchroedingerSimulator {
 			this.raw.data[position + 2] = Math.min(this.raw.data[position + 2], 200)
 		}
 		this.raw.data[position + 3] = 255
+	}
+
+	getProb(index) {
+		return this.r1[index] * this.r1[index] + this.i05[index] * this.i15[index]
 	}
 }
 
@@ -251,7 +247,7 @@ class SchroedingerGrapher {
 		let absMax = 0
 		for (let i = 0; i < this.width; i++) {
 			const index = i + this.parameters.offscreenBuffer + this.simulator.readY * this.simulator.width
-			const value = this.simulator.grid2[index]
+			const value = this.simulator.getProb(index)
 			this.data[i] += value * value
 			if (this.data[i] > absMax) absMax = this.data[i]
 		}
